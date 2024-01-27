9ad890cbde9c419fc35323cb5d7873db0a99b0e5
Patrick Ball <pball@hrdag.org>
2024-01-27 15:03:48 -0800

    adjusting test data path, not symlink

--- install.sh
+++ install.sh
@@ -14,7 +14,12 @@ if [[ $(sudo -v) ]] ; then
 	exit 1
 fi
 
-export PATH_TO_BTRSNAP_DEV=$(pwd)
+PATH_TO_BTRSNAP_DEV=$(pwd)
+INSTALLED_TEST_DATA_PATH=/usr/local/share/btrsnap
+
+if [[ ! -d $INSTALLED_TEST_DATA_PATH ]] ; then
+  sudo mkdir -p $INSTALLED_TEST_DATA_PATH
+fi
 
 sudo ln -sf $HOME/projects/hrdag/btrsnap/bin/_find-repo-files /usr/local/bin/
 sudo ln -sf $HOME/projects/hrdag/btrsnap/bin/_backend-test-fixture /usr/local/bin/
@@ -24,7 +29,7 @@ sudo ln -sf $HOME/projects/hrdag/btrsnap/bin/_init-btrsnap-repo /usr/local/bin/
 # probably should run btrsnap/bin/_stdize_timestamp on the data
 cd $PATH_TO_BTRSNAP_DEV/data && $PATH_TO_BTRSNAP_DEV/bin/_stdize_timestamp
 
-sudo mkdir -p /usr/local/share/btrsnap && \
-	cd $_ && ln -sf $PATH_TO_BTRSNAP_DEV/data .
+cd $INSTALLED_TEST_DATA_PATH && sudo rm -rf * 
+sudo cp -a $PATH_TO_BTRSNAP_DEV/data test
 
 # done.
